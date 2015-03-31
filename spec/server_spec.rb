d1159be35f1513120be7c6c3db1914ad6c616ca4
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-03-30 23:42:02 -0600

    Add specs for server requests

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,3 +1,5 @@
+require 'rack/test'
+
 describe Yars::Server do
   describe '.new' do
     it 'initializes' do
@@ -6,10 +8,30 @@ describe Yars::Server do
     end
   end
 
-  describe '.start' do
-    it 'boots a TCPServer' do
-      expect(TCPServer).to receive(:new).with('localhost', 8000)
-      expect { Yars::Server.start app: {} }.to raise_error
+  describe 'get' do
+    let(:app) do
+      ->(_env) { Rack::Response.new('Hello!', 200, 'Dag' => 'Gummit').finish }
+    end
+
+    let!(:server) do
+      Thread.new { Kernel.silence { Yars::Server.start(app: app) } }
+    end
+
+    after { server.kill }
+
+    it 'responds with 200 status' do
+      get '/'
+      expect(last_response.status).to eq 200
+    end
+
+    it 'responds with correct headers' do
+      get '/'
+      expect(last_response.headers['Dag']).to eq 'Gummit'
+    end
+
+    it 'responds with correct body' do
+      get '/'
+      expect(last_response.body).to eq 'Hello!'
     end
   end
 end
