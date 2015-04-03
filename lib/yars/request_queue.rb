a0da83176b6af00c47e1a6e563162467f3e5de8f
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-03 13:55:08 -0600

    Begin working on queue

--- lib/yars/request_queue.rb
+++ lib/yars/request_queue.rb
@@ -1,4 +1,32 @@
 module Yars
-  class RequestQueue < Queue
+  class AtomicQueue
+    def initialize
+      @head = Node.new pred: @tail
+      @tail = Node.new succ: @head
+    end
+
+    def <<(data)
+      new_node = Node.new data: data, succ: @tail.next, pred: @tail
+      @tail.next.prev = new_node
+      self
+    end
+
+    def pop
+    end
+
+    private
+
+    class Node
+      attr_accessor :data, :succ, :pred
+
+      def initialize(data: nil, succ: nil, pred: nil)
+        @data, @succ, @pred = data, succ, prec
+      end
+
+      alias_method :next, :succ
+      alias_method :prev, :pred
+    end
   end
+
+  class RequestQueue < Queue; end
 end
