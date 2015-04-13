179dad77a47c97fbb78c03613e2767468b1e5d65
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-13 17:48:31 -0600

    Create finish basic sequential queue

--- lib/yars/request_queue.rb
+++ lib/yars/request_queue.rb
@@ -1,17 +1,35 @@
 module Yars
   class AtomicQueue
+    attr_reader :size
+
     def initialize
       @head = Node.new pred: @tail
       @tail = Node.new succ: @head
+      @size = 0
     end
 
     def <<(data)
-      new_node = Node.new data: data, succ: @tail.next, pred: @tail
-      @tail.next.prev = new_node
+      node = Node.new data: data, succ: @tail.succ, pred: @tail
+
+      @head.pred = node if empty?
+      @tail.succ.pred = node
+      @tail.succ = node
+      @size += 1
+
       self
     end
 
     def pop
+      return nil if empty?
+
+      @head.pred.data.tap do
+        @head.pred = @head.pred.pred
+        @size -= 1
+      end
+    end
+
+    def empty?
+      @size == 0
     end
 
     private
@@ -20,11 +38,8 @@ module Yars
       attr_accessor :data, :succ, :pred
 
       def initialize(data: nil, succ: nil, pred: nil)
-        @data, @succ, @pred = data, succ, prec
+        @data, @succ, @pred = data, succ, pred
       end
-
-      alias_method :next, :succ
-      alias_method :prev, :pred
     end
   end
 
