b93e1540a1d471ba91c66675e71713f79ab04f98
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-29 13:29:35 -0600

    Make gh-pages branch

--- lib/yars/version.rb
+++
@@ -1,4 +0,0 @@
-# Ladies and gentlemen, it's Yars!
-module Yars
-  VERSION = '0.0.1'
-end
--- tasks/get.lua
+++
@@ -1,6 +0,0 @@
-math.randomseed(os.time())
-
-request = function()
-   wrk.headers["X-Counter"] = math.random(0,50)
-   return wrk.format(nil, path)
-end
